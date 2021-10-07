4fcefe10ed5c433bc40a1c666ca7fe33948f44ac
witoldmetel <witoldmetel@gmail.com>
2021-10-07 11:45:08 +0200

    add material ui theme

---
+++ src/theme/index.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react';
+
+import { CssBaseline } from '@mui/material';
+import {
+	createTheme,
+	ThemeProvider,
+	StyledEngineProvider,
+} from '@mui/material/styles';
+
+import { shape } from './shape';
+import { palette } from './palette';
+import { typography } from './typography';
+import { breakpoints } from './breakpoints';
+import { shadows, customShadows } from './shadows';
+
+type ThemeConfigProps = {
+	children: ReactNode;
+};
+
+export function ThemeConfig({ children }: ThemeConfigProps) {
+	const theme = createTheme({
+		palette: { ...palette, mode: 'light' },
+		shape,
+		typography,
+		breakpoints,
+		shadows,
+		customShadows,
+	});
+
+	return (
+		<StyledEngineProvider injectFirst>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				{children}
+			</ThemeProvider>
+		</StyledEngineProvider>
+	);
+}
