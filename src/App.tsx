c5add09d1e896c27f54c9700e0c99a73e7dc8018
witoldmetel <witoldmetel@gmail.com>
2021-10-21 11:52:32 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import { ThemeConfig } from './core/theme';
 import { NotistackProvider } from './core/components';
 import { Router } from './routes';
+import { getAuthState } from 'src/store/slices/auth';
+import { useSelector } from 'src/store/store';
 
 function App() {
+  const { isInitialized } = useSelector(getAuthState);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <ThemeConfig>
-      <NotistackProvider>
-        <Router />
-      </NotistackProvider>
+      <NotistackProvider>{isInitialized ? <Router /> : <div>Loader</div>}</NotistackProvider>
     </ThemeConfig>
   );
 }
