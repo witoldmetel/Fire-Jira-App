ab421b88187c3c6dec27a4bb9d88dc5a7282cd51
witoldmetel <witoldmetel@gmail.com>
2021-10-22 12:35:06 +0200

    Chore: remove faqs

--- src/core/layouts/constants.tsx
+++ src/core/layouts/constants.tsx
@@ -1,5 +1,5 @@
 import { Box } from '@mui/material';
-import { Help, VpnKey, ContactPage, EmojiPeople } from '@mui/icons-material';
+import { VpnKey, ContactPage, EmojiPeople } from '@mui/icons-material';
 
 import { PATH_AUTH, PATH_PAGE } from '../../routes/paths';
 
@@ -12,11 +12,6 @@ export const mainMenuConfig = [
   {
     title: 'Contact Me',
     icon: <ContactPage sx={{ width: 22, height: 22 }} />,
-    path: ''
-  },
-  {
-    title: 'FAQs',
-    icon: <Help />,
     path: ''
   },
   {
