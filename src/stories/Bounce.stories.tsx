be5f84ed5bab5c1b99007b135dd925a7495dc8db
witoldmetel <witoldmetel@gmail.com>
2021-10-26 12:10:48 +0200

    add bounce page

--- src/stories/Bounce.stories.tsx
+++ src/stories/Bounce.stories.tsx
@@ -1,10 +1,21 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { AnimationWrapper } from './AnimationWrapper';
-import { bounceIn, bounceOut, bounceOutUp } from '../core/components/animations/';
+import {
+  bounceOut,
+  bounceOutUp,
+  bounceOutDown,
+  bounceOutLeft,
+  bounceOutRight,
+  bounceIn,
+  bounceInUp,
+  bounceInDown,
+  bounceInLeft,
+  bounceInRight
+} from '../core/components/animations/';
 
 export default {
-  title: 'Bounce',
+  title: 'Fire Jira/Animations/Bounce',
   component: <AnimationWrapper />,
   // Sets the layout parameter component wide.
   parameters: {
@@ -24,7 +35,42 @@ BounceOutUp.args = {
   whileHover: { ...bounceOutUp.animate, transition: { duration: 2, repeat: Infinity } }
 };
 
+export const BounceOutDown = Template.bind({});
+BounceOutDown.args = {
+  whileHover: { ...bounceOutDown.animate, transition: { duration: 2, repeat: Infinity } }
+};
+
+export const BounceOutLeft = Template.bind({});
+BounceOutLeft.args = {
+  whileHover: { ...bounceOutLeft.animate, transition: { duration: 2, repeat: Infinity } }
+};
+
+export const BounceOutRight = Template.bind({});
+BounceOutRight.args = {
+  whileHover: { ...bounceOutRight.animate, transition: { duration: 2, repeat: Infinity } }
+};
+
 export const BounceIn = Template.bind({});
 BounceIn.args = {
   whileHover: { ...bounceIn.animate, transition: { duration: 2, repeat: Infinity } }
+};
+
+export const BounceInUp = Template.bind({});
+BounceInUp.args = {
+  whileHover: { ...bounceInUp.animate, transition: { duration: 2, repeat: Infinity } }
+};
+
+export const BounceInDown = Template.bind({});
+BounceInDown.args = {
+  whileHover: { ...bounceInDown.animate, transition: { duration: 2, repeat: Infinity } }
+};
+
+export const BounceInLeft = Template.bind({});
+BounceInLeft.args = {
+  whileHover: { ...bounceInLeft.animate, transition: { duration: 2, repeat: Infinity } }
+};
+
+export const BounceInRight = Template.bind({});
+BounceInRight.args = {
+  whileHover: { ...bounceInRight.animate, transition: { duration: 2, repeat: Infinity } }
 };
